81278f8e888b0b720637fd015f0e1b18a112f863
Aldrin Lim <aldrinlim81@gmail.com>
2024-08-10 21:56:35 +0800

    chore: Update main.tsx to use React.StrictMode and fix formatting

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 
 import { registerSW } from "virtual:pwa-register";
-import {
-  BrowserRouter as Router,
-} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 
 registerSW();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-      <Router>
-        <App />
-      </Router>
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
 );
